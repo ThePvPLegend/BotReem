55b0823aea00921cd4a4c2758fb1fe0cfc978680
ThePvPLegend <40651808+ThePvPLegend@users.noreply.github.com>
2019-10-16 14:29:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
+            if (message.content.startsWith(prefix ! "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
